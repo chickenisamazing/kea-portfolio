3984b81a38d403987655e62515de9e7259d3f4b4
chickenisamazing <dieswing@naver.com>
2025-04-24 17:15:08 +0900

    feat(Navbar): Add scroll event to category button

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,12 +1,36 @@
+"use client";
+
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  function scrollToSection(props: string) {
+    document.getElementById(props)?.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <nav className={styles.navbar}>
       <div className={styles["navbar-content"]}>
-        <a className={styles["about-me"]}>About Me</a>
-        <a className={styles.skills}>Skills</a>
-        <a className={styles.projects}>Projects</a>
+        <button
+          type="button"
+          onClick={() => scrollToSection("about-me-title")}
+          className={styles["about-me"]}
+        >
+          About Me
+        </button>
+        <button
+          type="button"
+          onClick={() => scrollToSection("skills-title")}
+          className={styles.skills}
+        >
+          Skills
+        </button>
+        <button
+          type="button"
+          onClick={() => scrollToSection("projects-title")}
+          className={styles.projects}
+        >
+          Projects
+        </button>
       </div>
     </nav>
   );

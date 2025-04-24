81ed267eab7b4dbecbce415b36ecdca47b6300aa
chickenisamazing <dieswing@naver.com>
2025-04-24 21:12:31 +0900

    feat(Navbar): Add scroll trigger to change category button state

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,14 +1,67 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import styles from "./Navbar.module.css";
 
-export default function Navbar() {
-  const [scrollLocation, getScrollLocation] =
-    useState<string>("about-me-title");
+const SECTION_ID_ARRAY = ["about-me-title", "skills-title", "projects-title"];
 
+export default function Navbar() {
+  const [scrollLocation, setScrollLocation] =
+    useState<string>("about-me-title");
+  const sectionTops = useRef<Record<string, number>>({});
+
+  // 3개 섹션들의 Y값 계산 및 저장
+  const calculateSectionTops = () => {
+    const newTops: Record<string, number> = {};
+    let changed = false;
+    SECTION_ID_ARRAY.forEach((id) => {
+      const element = document.getElementById(id);
+      if (element) {
+        const offsetTop = element.offsetTop;
+        if (sectionTops.current[id] !== offsetTop) {
+          changed = true;
+        }
+        newTops[id] = offsetTop;
+      }
+    });
+
+    if (changed || Object.keys(sectionTops.current).length === 0) {
+      sectionTops.current = newTops;
+      console.log("체크", sectionTops.current);
+    }
+  };
+
+  useEffect(() => {
+    calculateSectionTops();
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const activePoint = currentScrollY + window.innerHeight / 3;
+      let activeSection = SECTION_ID_ARRAY[0];
+
+      for (let i = SECTION_ID_ARRAY.length - 1; i >= 0; i--) {
+        const id = SECTION_ID_ARRAY[i];
+        const sectionTop = sectionTops.current[id];
+        if (sectionTop !== undefined && activePoint >= sectionTop) {
+          activeSection = id;
+          break;
+        }
+      }
+      if (scrollLocation !== activeSection) {
+        setScrollLocation(activeSection);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", calculateSectionTops);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", calculateSectionTops);
+    };
+  }, [scrollLocation]);
+
+  // 내비게이션 바 버튼 클릭시 해당 섹션으로 이동
   function scrollToSection(props: string) {
-    getScrollLocation(props);
     document.getElementById(props)?.scrollIntoView({ behavior: "smooth" });
   }
 

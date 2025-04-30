8d15686a1640ab7c8c8dae88758d6b3d1d749145
chickenisamazing <dieswing@naver.com>
2025-04-30 23:12:36 +0900

    refactor(Projects): Extract ProjectSkillsList component from ProjectCard.tsx

--- src/component/Projects/ProjectCard.tsx
+++ src/component/Projects/ProjectCard.tsx
@@ -1,19 +1,8 @@
-import { ComponentType } from "react";
-
 import Image from "next/image";
 
 import styles from "./ProjectCard.module.css";
 
-import HTMLIcon from "../icons/HTMLIcon";
-import CSSIcon from "../icons/CSSIcon";
-import CSSModulesIcon from "../icons/CSSModulesIcon";
-import JavaScriptIcon from "../icons/JavaScriptIcon";
-import TypeScriptIcon from "../icons/TypeScriptIcon";
-import ReactIcon from "../icons/ReactIcon";
-import NextIcon from "../icons/NextIcon";
-import TailwindCSSIcon from "../icons/TailwindCSSIcon";
-import PWAIcon from "../icons/PWAIcon";
-import ZustandIcon from "../icons/ZustandIcon";
+import ProjectSkillsList from "./ProjectSkillsList";
 
 export interface Stack {
   stack: string;
@@ -40,26 +29,6 @@ interface Project {
     blogPost: string;
   };
 }
-
-interface IconProps {
-  size?: number;
-  color?: string;
-  className?: string;
-  [key: string]: unknown;
-}
-
-const iconComponents: { [key: string]: ComponentType<IconProps> } = {
-  HTML: HTMLIcon,
-  CSS: CSSIcon,
-  "CSS Modules": CSSModulesIcon,
-  "Tailwind CSS": TailwindCSSIcon,
-  JavaScript: JavaScriptIcon,
-  TypeScript: TypeScriptIcon,
-  React: ReactIcon,
-  "Next.js App Router": NextIcon,
-  PWA: PWAIcon,
-  Zustand: ZustandIcon,
-};
 
 interface ProjectCardProps {
   project: Project;
@@ -112,30 +81,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
             <span className={styles.description}>{project.description}</span>
           </div>
 
-          {/* 기술 스택 */}
-          <div className={styles["skills-container"]}>
-            <span className={styles["project-label"]}>Skills</span>
-            <ul className={styles["skill-list"]}>
-              {project.skills.map((stack: Stack) => {
-                const specificIconComponent = iconComponents[stack.stack];
-                const IconToRender = specificIconComponent || ReactIcon;
-                return (
-                  <li
-                    style={{ background: stack.fontColor }}
-                    className={styles.skill}
-                    key={stack.stack}
-                  >
-                    <span className={styles["icon-align"]}>
-                      {IconToRender ? (
-                        <IconToRender className={styles.icon} color="white" />
-                      ) : null}
-                      {stack.stack}
-                    </span>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          <ProjectSkillsList project={project} />
 
           {/* 구현 기능 */}
           <div className={styles["feature-container"]}>

955c57f3552bbede8c128c5d67565f2d43bbb747
chickenisamazing <dieswing@naver.com>
2025-05-05 12:54:02 +0900

    refactor(AboutMe): Modify css class name and css code

--- src/component/AboutMe/AboutMe.tsx
+++ src/component/AboutMe/AboutMe.tsx
@@ -6,9 +6,12 @@ export default function AboutMe() {
   return (
     <section>
       <div className={styles.content}>
+        {/* 타이틀 */}
         <h2 id="about-me-title" className={styles.title}>
           About Me
         </h2>
+
+        {/* 프로필 이미지 */}
         <Image
           className={styles["profile-image"]}
           src="/assets/profile_image_penrose.svg"
@@ -17,43 +20,47 @@ export default function AboutMe() {
           height={288}
           priority
         />
+
+        {/* 링크 리스트 */}
         <dl className={styles["link-container"]}>
           <dt className={styles["link-category"]}>github.</dt>
-          <dd className={styles["link-link-group"]}>
+          <dd className={styles["link-item"]}>
             <a
               href="https://github.com/chickenisamazing"
               target="_blank"
               rel="noopener noreferrer"
-              className={styles["link"]}
+              className={styles["link-sentence"]}
             >
               github.com/chickenisamazing
             </a>
           </dd>
 
           <dt className={styles["link-category"]}>blog.</dt>
-          <dd className={styles["link-link-group"]}>
+          <dd className={styles["link-item"]}>
             <a
               href="https://unhandled.tistory.com"
               target="_blank"
               rel="noopener noreferrer"
-              className={styles["link"]}
+              className={styles["link-sentence"]}
             >
               unhandled.tistory.com
             </a>
           </dd>
 
           <dt className={styles["link-category"]}>email.</dt>
-          <dd className={styles["link-link-group"]}>
+          <dd className={styles["link-item"]}>
             <a
               href="mailto:kakapokachip@gmail.com"
               rel="noopener noreferrer"
-              className={styles["link"]}
+              className={styles["link-sentence"]}
             >
               kakapokachip@gmail.com
             </a>
           </dd>
         </dl>
-        <p className={styles.description}>
+
+        {/* 자기소개 슬로건 */}
+        <p className={styles.slogan}>
           <span className={styles.sentence}>저는</span>
           <span className={styles.sentence}>
             <span className={styles.thinking}>객관적 이성</span>을 추구하는
@@ -65,28 +72,29 @@ export default function AboutMe() {
             프론트엔드 개발자 <strong>케아</strong>입니다.
           </span>
         </p>
-        <p className={styles["description-2"]}>
-          <span className={styles["sentence-2"]}>저는</span>
-          <span className={styles["sentence-2"]}>
+
+        {/* 가치관 */}
+        <p className={styles.values}>
+          <span className={styles["values-sentence"]}>저는</span>
+          <span className={styles["values-sentence"]}>
             <strong>어떻게(How)</strong>만큼의 <strong>왜(Why)</strong>를
             추구합니다.
           </span>
-          <span className={styles["sentence-2"]}>
+          <span className={styles["values-sentence"]}>
             <strong>개념적 명확성</strong>을 추구합니다.
           </span>
         </p>
 
-        <p className={styles["description-2"]}>
-          <span className={styles["education-container"]}>
-            <span className={styles["education-title"]}>education</span>
-            <span className={styles["education-sentence"]}>
-              2023.07 ~ 2024.06
-            </span>
-            <span className={styles["education-sentence-2"]}>
+        {/* 교육사항 */}
+        <div className={styles["education-container"]}>
+          <h3 className={styles["education-title"]}>education</h3>
+          <dl className={styles["education-list"]}>
+            <dt className={styles["education-term"]}>
               <strong>삼성 청년 SW 아카데미 10기 수료</strong>
-            </span>
-          </span>
-        </p>
+            </dt>
+            <dd className={styles["education-sentence"]}>2023.07 ~ 2024.06</dd>
+          </dl>
+        </div>
       </div>
     </section>
   );

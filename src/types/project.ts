bd9db7b48c94bab76f0e2c581d9b3e17292d82dd
chickenisamazing <dieswing@naver.com>
2025-05-06 15:39:24 +0900

    refactor(types): Update Project type for Supabase data

--- src/types/project.ts
+++ src/types/project.ts
@@ -4,24 +4,35 @@ export interface Stack {
 }
 
 export interface Project {
-  projectId: number;
-  name: string;
-  type: string;
-  duration: string;
-  role: string;
-  image: {
-    src: string;
-    alt: string;
-  };
-  description: string;
-  skills: Stack[];
-  features: string[];
-  logo: string;
-  links: {
-    github: string;
-    domain: string;
-    blogPost: string;
-  };
+  project_id: number;
+  project_name: string;
+  project_type: string;
+  project_duration: string;
+  project_role: string;
+  project_description: string;
+  project_features: string[];
+  project_github_link: string;
+  project_domain_link: string;
+  project_blog_post_link: string;
+  project_logo: string;
+  // projectId: number;
+  // name: string;
+  // type: string;
+  // duration: string;
+  // role: string;
+  // image: {
+  //   src: string;
+  //   alt: string;
+  // };
+  // description: string;
+  // skills: Stack[];
+  // features: string[];
+  // logo: string;
+  // links: {
+  //   github: string;
+  //   domain: string;
+  //   blogPost: string;
+  // };
 }
 
 export interface ProjectCardProps {

baab2f8b26a194ee4c051c48514422350977bcfc
chickenisamazing <dieswing@naver.com>
2025-05-06 14:04:24 +0900

    chore(db): Add supabase to connect to external database

---
+++ src/lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+  throw new Error("Supabase 환경변수를 찾을 수 없습니다.");
+}
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;

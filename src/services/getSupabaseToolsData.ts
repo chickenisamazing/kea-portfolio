720db95d9ea0b51c3705c2a107ffcabe1e6b8a6b
chickenisamazing <dieswing@naver.com>
2025-05-06 17:40:53 +0900

    feat(Tools): Add getSupabaseToolsData function

---
+++ src/services/getSupabaseToolsData.ts
@@ -0,0 +1,16 @@
+import supabase from "@/lib/supabase";
+
+export default async function getSupabaseToolsData() {
+  const { data, error } = await supabase.from("tools").select();
+
+  // 컬럼명이 다르니 매핑하자
+  const mappedData = data?.map((item) => ({
+    stack_id: item.tool_id,
+    stack_name: item.tool_name,
+    stack_image_url: item.tool_image_url,
+    stack_color: item.tool_color,
+    stack_description: [item.tool_description],
+  }));
+
+  return { mappedData, error };
+}

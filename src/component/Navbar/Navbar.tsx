05c7c5c832b2637883467adc7be6248dead7716b
chickenisamazing <dieswing@naver.com>
2025-05-21 21:59:14 +0900

    fix(Navbar): Manage boolean variables as useRef to resolve stale closure issue

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -10,7 +10,9 @@ export default function Navbar() {
   const [activeSection, setActiveSection] = useState("about-me-title");
   const sectionRefs = useRef<Record<string, HTMLElement | null>>({});
 
-  const [doNotChange, setDoNotChange] = useState<boolean>(false);
+  // const [doNotChange, setDoNotChange] = useState<boolean>(false);
+
+  const doNotChangeRef = useRef(false);
 
   useLayoutEffect(() => {
     SECTION_ID_ARRAY.forEach((id) => {
@@ -19,18 +21,32 @@ export default function Navbar() {
 
     const observer = new IntersectionObserver(
       (entries) => {
+        // 여기서부터 안된다는건? 옵저버가 감지할 조건을 충족하지 못했다?
+        // console.log("1번실행되냐", doNotChangeRef);
+        console.log("isIntersecting : ", entries[0].isIntersecting);
+
         entries.forEach((entry) => {
+          // console.log("2번실행되냐", doNotChangeRef);
+          console.log(entry.isIntersecting, "????");
+          // 여기서부터 안된다는건. entry.isIntersecting이 거짓이다?
           if (entry.isIntersecting) {
             // 이걸 추가하면 aboutme -> projects로 갈 때 skills 버튼 스타일이 순간 바뀌는건 사라지나 스크롤 위치를 인식해서 버튼 스타일 바뀌는건 안됨 -> 수정해야함
-            if (doNotChange === false) {
+            //설마 클로저라서 false 였을때만 기억하나 얘?
+            // console.log("3.doNotChange:", doNotChangeRef);
+            if (!doNotChangeRef.current) {
               setActiveSection(entry.target.id);
             }
+
+            // if (doNotChange === false) {
+            //   setActiveSection(entry.target.id);
+            // }
           }
         });
       },
       {
+        // root: null이면 브라우저창
         root: null,
-        threshold: 0.3,
+        threshold: 0.0,
       }
     );
 
@@ -45,7 +61,8 @@ export default function Navbar() {
   }, []);
 
   const scrollTo = (id: string) => {
-    setDoNotChange(true);
+    // setDoNotChange(true);
+    doNotChangeRef.current = true;
 
     let target = sectionRefs.current[id];
 
@@ -57,7 +74,8 @@ export default function Navbar() {
     target?.scrollIntoView({ behavior: "smooth" });
     setActiveSection(id);
     setTimeout(() => {
-      setDoNotChange(false);
+      // setDoNotChange(false);
+      doNotChangeRef.current = false;
     }, 700);
   };
 

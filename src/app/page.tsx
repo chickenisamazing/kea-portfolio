9db4101ce777cc7abeb92faf3acbc6a3d35269a6
chickenisamazing <dieswing@naver.com>
2025-05-21 21:53:14 +0900

    refactor(Stacks): Add StacksSection component and delete dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,13 @@
-// SSG -> SSR TEST
-// export const dynamic = "force-dynamic";
-
-// Lazy loading (자식 컴포넌트)
-import dynamicImport from "next/dynamic";
-
-import type { Stack } from "@/types/stacks";
-
 import AboutMe from "@/component/AboutMe/AboutMe";
 
-import getSkillsData from "@/services/getSkillsData";
-import getToolsData from "@/services/getToolsData";
-
-const Stacks = dynamicImport(() => import("@/component/Stacks/Stacks"));
-const Projects = dynamicImport(() => import("@/component/Projects/Projects"));
+import StacksSection from "@/component/Stacks/StacksSection";
+import Projects from "@/component/Projects/Projects";
 
 export default async function Home() {
-  const { data } = await getSkillsData();
-  const { mappedData } = await getToolsData();
-
   return (
     <div>
       <AboutMe />
-      <Stacks title="Skills" data={data ?? []} />
-      <Stacks title="Tools" data={(mappedData ?? []) as Stack[]} />
+      <StacksSection />
       <Projects />
     </div>
   );

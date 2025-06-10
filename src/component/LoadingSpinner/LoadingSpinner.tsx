81a2ba14d23c562acc1bf0d87fe8cf82faf59bea
chickenisamazing <dieswing@naver.com>
2025-06-10 12:17:14 +0900

    refactor(loading): Move entire styling to loading spinner component

--- src/component/LoadingSpinner/LoadingSpinner.tsx
+++ src/component/LoadingSpinner/LoadingSpinner.tsx
@@ -1,5 +1,9 @@
 import styles from "./LoadingSpinner.module.css";
 
 export default function Loader() {
-  return <div className={styles.loader}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader} />
+    </div>
+  );
 }

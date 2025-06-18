63d4921ca1c0b286e0574770dfb043469d0ffb2a
chickenisamazing <dieswing@naver.com>
2025-06-18 14:07:19 +0900

    design(not-found): Update not-found page css styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,10 @@ export default function NotFound() {
     <div className={styles.content}>
       <h2>404 Not Found</h2>
       <p>이 페이지는 존재하지 않아요 !</p>
-      <Link href="/">메인 페이지로 돌아가기</Link>
+      <Link href={`/`} className={styles["circle-wrapper"]}>
+        <p className={styles["return-to-main"]}>메인 페이지로 돌아가기</p>
+        <div id="hover-icon" className={styles.circle} />
+      </Link>
     </div>
   );
 }
